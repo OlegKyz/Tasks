9634c64b4013074e298b904701b458996347a265
Oleg Kuznetsov <oleg.kyznetzov@mail.ru>
2022-07-27 11:19:05 +0400

    test db

--- src/main/java/ru/kuznetcov/oleg/App.java
+++ src/main/java/ru/kuznetcov/oleg/App.java
@@ -1,13 +1,45 @@
 package ru.kuznetcov.oleg;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App 
 {
+	private static final String driverName = "com.mysql.jdbc.Driver";
+	private static final String connectionString = "jdbc:mysql://localhost/Tasks_test_db";
+	private static final String login = "root";
+	private static final String password = "";
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println(System.getProperty("java.class.path"));
+        testRun();
+    }
+
+    private static void testRun() {
+    	try {
+    		Class.forName(driverName);
+    	} catch (ClassNotFoundException e) {
+    		System.out.println("Can't get class");
+    		e.printStackTrace();
+    		return;
+    	}
+    	Connection connection = null;
+    	try {
+    		connection = DriverManager.getConnection(connectionString, login, password);
+    	} catch (SQLException e) {
+    		System.out.println("Can't get connection");
+    		e.printStackTrace();
+    		return;
+    	}
+    	try {
+    		connection.close();
+    	} catch (SQLException e) {
+    		System.out.println("Can't close connection");
+    		e.printStackTrace();
+    		return;
+    	}
+    	System.out.println("testRun - OK");
     }
 }
---
+++ src/main/resources/test_db_init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE Tasks_test_db;
+
+CREATE TABLE Test_table
+(
+	Id INT,
+ 	Str Varchar(3)
+);
+
+INSERT INTO Test_table
+VALUES
+(1, 'aaa'),
+(2, 'bbb'),
+(3, 'ccc');
