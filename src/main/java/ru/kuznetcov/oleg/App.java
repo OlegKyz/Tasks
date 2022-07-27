33434db8f0513df517f2708282167d146a8cd440
Oleg Kuznetsov <oleg.kyznetzov@mail.ru>
2022-07-27 09:27:34 +0400

    init

---
+++ src/main/java/ru/kuznetcov/oleg/App.java
@@ -0,0 +1,13 @@
+package ru.kuznetcov.oleg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
